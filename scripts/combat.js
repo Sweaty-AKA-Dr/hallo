22b3a9313e394133a46774e7ef0997dcac402677
Sweaty-AKA-Dr <109566073+Sweaty-AKA-Dr@users.noreply.github.com>
2023-03-09 00:09:20 -0500

    Update combat.js

--- scripts/combat.js
+++ scripts/combat.js
@@ -1,18 +1,19 @@
 import { world } from '@minecraft/server'
-import { setTickTimeout } from './functions'
+import { setTickTimeout } from './functions'import { world } from "@minecraft/server";
+import { setTickTimeout } from "./functions";
 
 world.events.entityHurt.subscribe((data) => {
-  const attacker = data.damageSource.damagingEntity
-  const attacked = data.hurtEntity
-  if (attacker.typeId == 'minecraft:player' && attacked.typeId == 'minecraft:player') {
-    attacker.addTag('in_combat')
-    attacked.addTag('in_combat')
+  const attacker = data.damageSource.damagingEntity;
+  const attacked = data.hurtEntity;
+  if (attacker.typeId == "minecraft:player" && attacked.typeId == "minecraft:player") {
+    attacker.addTag("in_combat");
+    attacked.addTag("in_combat");
     setTickTimeout(() => {
-      attacker.removeTag('in_combat')
-      attacked.removeTag('in_combat')
-    }, 200)
-  } else if (attacker.typeId == 'minecraft:player' && attacked.typeId == 'minecraft:player' && attacked.getComponent('minecraft:health').current <= 0) {
-    attacker.removeTag('in_combat')
-    attacked.removeTag('incombat')
-  } else return
-})
+      attacker.removeTag("in_combat");
+      attacked.removeTag("in_combat");
+    }, 200);
+  } else if (attacker.typeId == "minecraft:player" && attacked.typeId == "minecraft:player" && attacked.getComponent("minecraft:health").current <= 0) {
+    attacker.removeTag("in_combat");
+    attacked.removeTag("incombat");
+  } else return;
+});
