c5111c405972705d46077845e7050feb4dbc04ba
Sweaty-AKA-Dr <109566073+Sweaty-AKA-Dr@users.noreply.github.com>
2023-03-09 00:37:13 -0500

    Update display.js

--- scripts/display.js
+++ scripts/display.js
@@ -11,7 +11,8 @@ system.runSchedule(() => {
     const kdr_decimals = getScore(player, "KDR-Decimals");
     const members = metricNumbers(getScore(player, "Members"));
     const players = getScore(player, "POnline");
-    player.onScreenDisplay.setTitle(` §fName §c-\n§b ${player.name}\n §fBalance §c-\n §a$§b${money}\n §fTime Played §c-\n §b${time} Hours\n §fKills §c-\n §b${kills}\n §fDeaths §c-\n §b${deaths}\n/ §fK/D §c-\n §b${kdr}.${kdr_decimals}%\n §f---------------\n §bRealm Info\n§f ---------------\n §fRealm Code §c-\n §8[§bJG8rwHwx3_s§8]\n §fDiscord Code §c-\n §8[§bceQPkvrJpr§8]`);
-    player.onScreenDisplay.setActionBar(`§l§c§c§f Total Members §c-§b ${members} \n§c §fPlayers Online§c - §b ${players}/11§e\n Game Version 1.10.0\n(Anti C-Log)\n§7§oDo +help to see the \ncommand list`);
+    player.onScreenDisplay.setTitle(`§b§lLegends Skygen§r\n\n §fName §c-\n§b ${player.name}\n §fBalance §c-\n §a$§b${money}\n/ §fK/D §c-\n §b${kdr}.${kdr_decimals}%\n §f---------------\n §bRealm Info\n§f ---------------\n §fRealm Code §c-\n §8[§bJG8rwHwx3_s§8]\n §fDiscord Code §c-\n §8[§bceQPkvrJpr§8]`);
+    player.onScreenDisplay.setActionBar(`§l§c§c§f Total Members §c-§b ${members} \n§c §fPlayers Online§c - §b ${players}/11§e\n Game Version 1.10.0\n(Anti C-Log)\n§7§oDo +help to see the \ncommand list`;
   });
 });
+
