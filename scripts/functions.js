1994406c241831123db4deebc79748fb950ab027
Sweaty-AKA-Dr <109566073+Sweaty-AKA-Dr@users.noreply.github.com>
2023-03-09 00:11:50 -0500

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,40 +1,40 @@
-import { world } from '@minecraft/server'
+import { world } from "@minecraft/server";
 
 export function setTickTimeout(callback, tick, loop = false) {
-  let cT = 0
+  let cT = 0;
   const tE = world.events.tick.subscribe((data) => {
-    if (cT === 0) cT = data.currentTick + tick
+    if (cT === 0) cT = data.currentTick + tick;
     if (cT <= data.currentTick) {
       try {
-        callback()
+        callback();
       } catch (e) {
-        console.warn(`${e} : ${e.stack}`)
+        console.warn(`${e} : ${e.stack}`);
       }
-      if (loop) cT += tick
-      else world.events.tick.unsubscribe(tE)
+      if (loop) cT += tick;
+      else world.events.tick.unsubscribe(tE);
     }
-  })
+  });
 }
 
 export function getScore(target, value, useZero = true) {
   try {
-    const objective = world.scoreboard.getObjective(value)
-    if (typeof target == 'string')
+    const objective = world.scoreboard.getObjective(value);
+    if (typeof target == "string")
       return objective.getScore(
         objective
           .getParticipants()
           .find((player) => player.displayName == target)
-      )
-    return objective.getScore(target.scoreboard)
+      );
+    return objective.getScore(target.scoreboard);
   } catch {
-    return useZero ? 0 : NaN
+    return useZero ? 0 : NaN;
   }
 }
 
 export function metricNumbers(value) {
-  const types = ['', 'k', 'M', 'B']
-  const selectType = (Math.log10(value) / 3) | 0
-  if (selectType == 0) return value
-  let scaled = value / Math.pow(10, selectType * 3)
-  return scaled.toFixed(2) + types[selectType]
+  const types = ["", "k", "M", "B"];
+  const selectType = (Math.log10(value) / 3) | 0;
+  if (selectType == 0) return value;
+  let scaled = value / Math.pow(10, selectType * 3);
+  return scaled.toFixed(2) + types[selectType];
 }
